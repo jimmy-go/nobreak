3202193ee64b3bace89a84dfc3d1e9d752ff465e
Ivan Jaimes <ivan.jaimes@iver.mx>
2018-02-01 12:24:16 -0600

    Add musl binary path

--- scripts/build_amd64.sh
+++ scripts/build_amd64.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
+
 set -o errexit
 set -o nounset
 
 BIN=bin/nobreak_amd64
 
-ls -lha /usr/local/musl
-
-CC=/usr/local/musl/bin/musl-gcc CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -a \
+CC=/usr/bin/x86_64-alpine-linux-musl-gcc-6.3.0 CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -a \
     --ldflags '-linkmode external -extldflags "-static"' \
     -o $BIN ./cmd/...
