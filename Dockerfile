1c1ad7e9660537b5723c4e66c0908a2584e2389b
Angel Del Castillo <jubu201504@gmail.com>
2018-01-31 23:22:26 -0600

    Build attempt.

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM golang:1.9.3-alpine
 
-RUN apk --update --no-cache add curl bash git alpine-sdk
+RUN apk --update --no-cache add curl bash git alpine-sdk util-linux gcc musl-dev
 
 RUN curl https://glide.sh/get | sh
 
--- scripts/build_amd64.sh
+++ scripts/build_amd64.sh
@@ -4,5 +4,8 @@ set -o nounset
 
 BIN=bin/nobreak_amd64
 
-CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -a \
+ls -lha /usr/local/musl
+
+CC=/usr/local/musl/bin/musl-gcc CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -a \
+    --ldflags '-linkmode external -extldflags "-static"' \
     -o $BIN ./cmd/...
